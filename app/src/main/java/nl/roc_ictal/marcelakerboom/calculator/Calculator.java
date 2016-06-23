60bc18c2f7ddf73781303b882e3050a04056d6f1
marcello.akerboom <marcello.akerboom@gmail.com>
2016-06-23 12:25:34 +0200

    Repeating evaluation works.

--- app/src/main/java/nl/roc_ictal/marcelakerboom/calculator/Calculator.java
+++ app/src/main/java/nl/roc_ictal/marcelakerboom/calculator/Calculator.java
@@ -6,14 +6,14 @@ import java.math.BigDecimal;
  * Created by marcelakerboom on 23/06/16.
  */
 public class Calculator {
-    private String entry;
-    private String operand;
-    private String augment;
+    private BigDecimal entry;
+    private BigDecimal operand;
+    private BigDecimal augment;
     private Operator operator;
+    private BigDecimal memoryEntry;
 
     private boolean hasDot;
     private boolean isResult;
-    private String memoryEntry;
 
     public enum Operator {
         ADD, SUBTRACT, DIVIDE, MULTIPLY
@@ -29,7 +29,7 @@ public class Calculator {
     }
 
     public void append(char digit) {
-        String entry = this.entry;
+        String entry = this.entry.toString();
         if (this.isResult) {
             entry = "";
         }
@@ -38,10 +38,10 @@ public class Calculator {
     }
 
     public void appendDot() {
-        if(this.isResult) {
+        if (this.isResult) {
             this.clearEntry();
         }
-        if(!this.hasDot) {
+        if (!this.hasDot) {
             this.setEntry(this.entry + ".");
         }
     }
@@ -49,42 +49,46 @@ public class Calculator {
     public void operate(Operator operator) {
         this.operator = operator;
         this.operand = this.entry;
+        this.augment = null;
         this.isResult = true;
     }
 
     public void evaluate() {
-        if(this.isResult) {
-            this.operand = this.entry;
-        } else {
-            this.augment = this.entry;
-        }
-        BigDecimal operand = new BigDecimal(this.operand);
-        BigDecimal augment = new BigDecimal(this.augment);
         BigDecimal result;
 
-        switch (this.operator) {
-            case ADD:
-                result = operand.add(augment);
-                break;
-            case SUBTRACT:
-                result = operand.subtract(augment);
-                break;
-            case MULTIPLY:
-                result = operand.multiply(augment);
-                break;
-            case DIVIDE:
-                result = operand.divide(augment);
-                break;
-            default:
-                result = augment;
-                break;
+        if (this.operator != null) {
+            if (this.isResult && this.augment != null) {
+                this.operand = this.entry;
+            } else {
+                this.augment = this.entry;
+            }
+
+            switch (this.operator) {
+                case ADD:
+                    result = operand.add(augment);
+                    break;
+                case SUBTRACT:
+                    result = operand.subtract(augment);
+                    break;
+                case MULTIPLY:
+                    result = operand.multiply(augment);
+                    break;
+                case DIVIDE:
+                    result = operand.divide(augment);
+                    break;
+                default:
+                    result = augment;
+                    break;
+            }
+        } else {
+            result = this.entry;
         }
 
         this.setEntry(result.toString(), true);
     }
 
     public void clearEntry() {
-        this.entry = "0";
+        this.entry = new BigDecimal("0");
         this.hasDot = false;
         this.isResult = true;
     }
@@ -101,7 +105,7 @@ public class Calculator {
     }
 
     public String getEntry() {
-        return this.entry;
+        return this.entry.toString();
     }
 
     public void setEntry(String entry) {
@@ -109,19 +113,20 @@ public class Calculator {
     }
 
     public void setEntry(String entry, boolean isResult) {
+        this.setEntry(new BigDecimal(entry), isResult);
+    }
+
+    public void setEntry(BigDecimal entry, boolean isResult) {
         this.entry = entry;
         this.isResult = isResult;
     }
 
     public void memoryAdd() {
-        BigDecimal memoryEntry = new BigDecimal(this.memoryEntry);
-        BigDecimal entry = new BigDecimal(this.entry);
         memoryEntry = memoryEntry.add(entry);
-        this.memoryEntry = memoryEntry.toString();
-        this.setEntry(this.memoryEntry, true);
+        this.setEntry(memoryEntry, true);
     }
 
     public void memoryClear() {
-        this.memoryEntry = "0";
+        this.memoryEntry = new BigDecimal("0");
     }
 }
